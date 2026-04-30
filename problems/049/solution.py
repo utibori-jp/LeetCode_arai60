e28bd846d8f00a22435b75a18639896b48c10776
utibori-jp <kouki.utibori@gmail.com>
2026-05-01 08:39:25 +0900

    feat(049): add char frequency count approach as second solution

--- problems/049/solution.py
+++ problems/049/solution.py
@@ -9,16 +9,22 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
     # 初回回答（2026/04/29）
-        anagram_map = defaultdict(list)
+        # anagram_map = defaultdict(list)
 
-        for word in strs:
-            key = "".join(sorted(word))
-            anagram_map[key].append(word)
+        # for word in strs:
+        #     key = "".join(sorted(word))
+        #     anagram_map[key].append(word)
         
-        return list(anagram_map.values())
+        # return list(anagram_map.values())
 
     # 2回目（2026/04/30）
-
+        anagram_map = defaultdict(list)
+        for word in strs:
+            char_freq = [0] * 26
+            for ch in word:
+                char_freq[ord(ch) - ord('a')] += 1
+            anagram_map[tuple(char_freq)].append(word)
+        return list(anagram_map.values())
 
 
 if __name__ == "__main__":
